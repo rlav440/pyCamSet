fully running but incorrect values for just the loss

pyCamSet/optimisation/abstract_function_blocks.py
@@ -107,8 +107,6 @@ class optimisation_function:
         self.block_param_inds = [self.unique_link_inds.index(fb.params) for fb in self.function_blocks]
 
         self.param_starts = np.cumsum([0] + [b.n_params for b in self.unique_link_inds])
-
-
         self.n_outs = np.array([b.num_out for b in self.function_blocks])
         self.n_inps = np.array([b.num_inp for b in self.function_blocks])
         self.n_params = np.array([b.params.n_params for b in self.function_blocks])
@@ -164,9 +162,6 @@ class optimisation_function:
         working_memories = np.array(self.working_memories)
         n_params  = np.array(self.n_params)
         n_outs = np.array(self.n_outs)
-
-        self.loss_jac = loss_jac
-        self.loss_fun = loss_fun
 
 
         @njit(cache=True)
@@ -229,8 +224,34 @@ class optimisation_function:
             return jac
         return make_jacobean
 
-    def _make_lookup_fn(self):
-        return lambda x:x
+    def _make_lookup_fn(self, detection_data):
+        """
+        builds a lookup function that takes the cam, im and feature number and returns the correct params for each feature.
+        """
+
+        #takes in the input function blocks.
+        #finds all of the unique params used within the optimisation
+        
+        starts, block_n_params, param_inds, key_type = make_param_struct(self.function_blocks, detection_data) 
+
+        # point locations within the jacobean matrix that will be used
+        param_len = np.sum(block_n_params)
+        param_slices = self.param_slices
+
+        @njit
+        def lookups(param_line):
+            """
+            Constructs an array that indexes the input param structure for the param elements relevant to this computation
+            """
+            param_data = np.empty(param_len)
+            nblocks = len(param_inds)
+            for idb in range(nblocks):
+                s_num = param_line[key_type[idb]] #the index value of the associated parameter
+                p_ind = param_inds[idb] # maps the param to it's index in the unique params
+                start = starts[p_ind] + s_num * block_n_params[p_ind] #and the associated change in the start location
+                param_data[param_slices[2*idb]:param_slices[2*idb + 1]] = np.arange(start=int(start), stop=int(start + block_n_params[p_ind]))
+            return param_data
+        return lookups
 
     def _make_loss_per_line_function(self) -> Callable:
         self._prep_for_computation()
@@ -261,12 +282,8 @@ class optimisation_function:
 
         self._prep_for_computation()
 
-        line_loss_fn = self._make_loss_per_line_function()
-        lookup_fn = self._make_lookup_fn()
-
-        #take and reshape the detections?
         d_shape = detections.shape
-        p_shape = (threads, int(np.ceil(d_shape[0]/threads)), *d_shape)
+        p_shape = (threads, int(np.ceil(d_shape[0]/threads)), *d_shape[1:])
         detection_data = np.resize(detections, p_shape)
 
         # get the shape of the jacobean from the data
@@ -275,12 +292,14 @@ class optimisation_function:
         out_mem = self.out_mem_req
         wrk_mem = self.wrk_mem_req
 
+        line_n_params = self.param_line_length
+
         use_template = self.templated and (template is not None)
         if self.templated and not use_template:
             raise ValueError("A templated optimisation was defined, but no template data was given to create the loss function")
         t_data: np.ndarray = template if use_template else np.zeros(3)
-
-        @njit(cache=True)
+#
+        @njit
         def full_loss(params, per_line_loss, loss_fns, lookup_make):
             losses = np.empty((n_threads, n_lines, 2))
             for i in prange(n_threads):
@@ -288,20 +307,30 @@ class optimisation_function:
                 inp_memory = np.empty(inp_mem)
                 out_memory = np.empty(out_mem)
                 wrk_memory = np.empty(wrk_mem)
+                param_lst = np.empty(line_n_params)
 
                 for ii in range(n_lines):
                     datum = detection_data[i, ii]
                     lookup = lookup_make(datum)
-                    param_lst = params[lookup]
+                    for idl, l in enumerate(lookup):
+                        param_lst[idl] = params[int(l)]
+                    # param_lst = params[lookup.astype(int)]
                     if use_template:
-                        inp_memory[:3] = t_data[datum[2]] 
+                        inp_memory[:3] = t_data[int(datum[2])] 
                     line_loss = per_line_loss(param_lst, inp_memory, out_memory, wrk_memory, loss_fns)
                     losses[i, ii] = line_loss[:2]
             return losses
 
         #have to return a python function that wraps the code in order to get the right result.
+        line_loss_fn = self._make_loss_per_line_function()
+        lookup_fn = self._make_lookup_fn(detection_data=detections)
+        loss_fun = List.empty_list(self.ftemplate)
+
+        for block in self.function_blocks:
+            loss_fun.append(block.compute_fun)# a bunch of function objects to call
+
         def loss_evaluator(params):
-            return full_loss(params, line_loss_fn, self.loss_fun, lookup_fn).flatten() 
+            return full_loss(params, line_loss_fn, loss_fun, lookup_fn).flatten() 
 
         return loss_evaluator
     
@@ -385,7 +414,7 @@ class abstract_function_block(ABC):
         raise ValueError(f"could not combine function block with {other}")
 
 def make_param_struct(
-        function_blocks : list[abstract_function_block], detection_data
+        function_blocks : list[Type[abstract_function_block]], detection_data
     ) -> tuple[np.ndarray,np.ndarray, np.ndarray, np.ndarray]:
     """
     Takes an input function block definition and detection data, then defines a structure
@@ -409,20 +438,29 @@ def make_param_struct(
         key_type.PER_KEY:max_keys,
     }
 
+    print(param_numbers)
+
     #TODO account for the mod function here, modifying the numbers of params
 
     # find all of the unique instances of link indicators
-    unique_link_inds = list(set([fb.params for fb in function_blocks]))
+    unique_link_inds = []
+    for fb in function_blocks:
+        if not fb.params in unique_link_inds:
+            unique_link_inds.append(fb.params)
+
     block_param_inds = [unique_link_inds.index(fb.params) for fb in function_blocks]
     param_starts = []
     param_offset = []
     associated = []
-    starting_point = 0
+    starting_point = 0 
     for link_ind in unique_link_inds:
         param_starts.append(starting_point)
         param_offset.append(link_ind.n_params)
-        associated.append(int(link_ind.n_params))
+        associated.append(int(link_ind.link_type))
         starting_point += link_ind.n_params * param_numbers[link_ind.link_type] 
+    print("testing")
+    print(param_offset)
+    print(associated)
 
     return np.array(param_starts), np.array(param_offset), np.array(block_param_inds), np.array(associated)
     

pyCamSet/optimisation/optimisation_handling.py
@@ -118,7 +118,7 @@ def run_bundle_adjustment(param_handler: TemplateBundleHandler,
     :param param_handler: The parameter handler that represents the optimisation
     :return: The output of the calibration and the argmin defined CameraSet
     """
-    loss_fn, init_params = make_optimisation_function(
+    loss_fn, bundle_jac, init_params = make_optimisation_function(
         param_handler, threads
     )
 
@@ -127,6 +127,9 @@ def run_bundle_adjustment(param_handler: TemplateBundleHandler,
     logging.info(f'found {len(init_params):.2e} parameters')
     logging.info(f'found {len(init_err):.2e} control points')
     logging.info(f'Initial Euclidean error: {init_euclid:.2f} px')
+
+    test = lambda : loss_fn(init_params)
+    gu.benchmark(test, repeats=100)
 
     if (init_euclid > 150) or (init_euclid == np.nan):
         logging.critical("Found worryingly high/NaN initial error: check that the initial parametisation is sensible")
@@ -144,6 +147,7 @@ def run_bundle_adjustment(param_handler: TemplateBundleHandler,
         # tr_solver='lsmr',
         # jac_sparsity=sparsity,
         # loss='soft_l1',
+        jacobian = bundle_jac, #pass the function for the jacobian if it exists
         max_nfev=100,
         x_scale='jac',
     )

pyCamSet/optimisation/template_handler.py
@@ -68,10 +68,10 @@ class TemplateBundlePrimitive:
         intr_data = params[self.extr_end:self.intr_end].reshape((-1, 4))
         dst_data = params[self.intr_end:self.dst_end].reshape((-1, 5))
 
-        ch.fill_pose(pose_data, self.poses, self.poses_unfixed)
-        ch.fill_extr(extr_data, self.extr, self.extr_unfixed)
-        ch.fill_intr(intr_data, self.intr, self.intr_unfixed)
-        ch.fill_dst(dst_data, self.dst, self.dst_unfixed)
+        ch.fill_flat(pose_data, self.poses, self.poses_unfixed)
+        ch.fill_flat(extr_data, self.extr, self.extr_unfixed)
+        ch.fill_flat(intr_data, self.intr, self.intr_unfixed)
+        ch.fill_flat(dst_data, self.dst, self.dst_unfixed)
         return np.concatenate((self.intr, self.dst), axis=1), self.extr, self.poses
 
 class TemplateBundleHandler:
@@ -121,9 +121,9 @@ class TemplateBundleHandler:
         n_poses = detection.max_ims
         n_cams = camset.get_n_cams()
 
-        self.poses = np.zeros((n_poses, 12))
-        self.extr = np.zeros((n_cams, 4, 4))
-        self.intr = np.zeros((n_cams, 3, 3))
+        self.poses = np.zeros((n_poses, 6))
+        self.extr = np.zeros((n_cams, 6))
+        self.intr = np.zeros((n_cams, 4))
         self.dst = np.zeros((n_cams, 5))
 
         self.extr_unfixed = np.array(['ext' not in self.fixed_params.get(cam_name, {}) for cam_name in self.cam_names])
@@ -148,12 +148,19 @@ class TemplateBundleHandler:
         # we define an abstract function block to handle the calibration
         self.op_fun: afb.optimisation_function = fb.projection() + fb.extrinsic3D() + fb.template_points()
 
+    def can_make_jac(self):
+        return False
+
     def make_loss_fun(self, threads):
-        temp_loss = self.op_fun.make_full_loss_fn(self.detection.get_data(), threads)
+        
+        #flatten the object shape
+        obj_data = self.target.point_data.reshape((-1, 3))
+
+        temp_loss = self.op_fun.make_full_loss_fn(self.detection.get_data(), threads, template=obj_data)
         def loss_fun(params):
             inps = self.get_bundle_adjustment_inputs(params)
             param_str = self.op_fun.build_param_list(*inps)
-            return temp_loss(param_str)
+            return temp_loss(param_str, )
         return loss_fun
 
     def make_loss_jac(self, threads):
@@ -204,7 +211,7 @@ class TemplateBundleHandler:
             if 'dst' in self.fixed_params.get(cam_name, {}):
                 self.dst[idx] = self.fixed_params[cam_name]['dst']
 
-    def get_bundle_adjustment_inputs(self, x) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    def get_bundle_adjustment_inputs(self, x, make_points=False) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
         """
         This function uses the state of the parameter handler, and the given params
         to build np arrays that describe:
@@ -218,15 +225,19 @@ class TemplateBundleHandler:
 
         :param x: The input optimisation parameters.
         """
-        x = self.parse_extra_params_and_setup(x)
-        poses, extr, intr, dst = self.bundlePrimitive.return_bundle_primitives(x)
-        proj = intr @ extr[:, :3, :]
-        im_points = np.empty((len(poses), *self.point_data.shape))
-        for idx, pose in enumerate(poses):
-            ch.n_htform_broadcast_prealloc(self.point_data, pose, im_points[idx])
-        
-        im_points = np.reshape(im_points, (len(poses), -1, 3))
-        return proj, intr, dst, im_points
+        proj, extr, poses = self.bundlePrimitive.return_bundle_primitives(x)
+        # proj = intr @ extr[:, :3, :]
+        if make_points:
+            im_points = np.empty((len(poses), *self.point_data.shape))
+            for idx, pose in enumerate(poses):
+                blank = np.zeros((12))
+                ch.n_e4x4_flat_INPLACE(pose, blank)
+                ch.n_htform_broadcast_prealloc(self.point_data, blank, im_points[idx])
+
+            im_points = np.reshape(im_points, (len(poses), -1, 3))
+            return im_points
+        # return proj, intr, dst, im_points
+        return proj, extr, poses
 
     def find_and_exclude_transform_outliers(self, per_im_error): 
         """
@@ -323,7 +334,6 @@ class TemplateBundleHandler:
             if intr_unfixed:
                 param_array.append(np.squeeze(cams[idc].distortion_coefs))
         param_array = np.concatenate(param_array, axis=0)
-        param_array = self.add_extra_params(param_array)
         return param_array
 
     def get_camset(self, x, return_pose=False) -> CameraSet | tuple[CameraSet, np.ndarray]:
@@ -335,16 +345,21 @@ class TemplateBundleHandler:
         :return: Either a CameraSet, or a CameraSet and a list of object poses.
         """
 
-        x = self.parse_extra_params_and_setup(x)
 
         new_cams = copy(self.camset)
-        poses, extr, intr, dst = self.bundlePrimitive.return_bundle_primitives(x)
+        proj, extr, poses = self.bundlePrimitive.return_bundle_primitives(x)
 
         for idc, cam_name in enumerate(self.cam_names):
+            blank_intr = np.eye(3)
+
+            blank_intr[0, 0] = proj[idc][0]
+            blank_intr[0, 2] = proj[idc][1]
+            blank_intr[1, 1] = proj[idc][2]
+            blank_intr[1, 2] = proj[idc][3]
             temp_cam: Camera = new_cams[cam_name]
-            temp_cam.extrinsic = extr[idc]
-            temp_cam.intrinsic = intr[idc]
-            temp_cam.distortion_coefs = dst[idc]
+            temp_cam.extrinsic = gu.make_4x4h_tform(extr[idc][:3], extr[idc][3:])
+            temp_cam.intrinsic = blank_intr
+            temp_cam.distortion_coefs = proj[idc][4:]
             temp_cam._update_state()
         if not return_pose:
             return new_cams
@@ -384,7 +399,7 @@ class TemplateBundleHandler:
         
         :param params: The input parameters to an optimisation.
         """
-        _, _, _, obj_points = self.get_bundle_adjustment_inputs(params)
+        obj_points = self.get_bundle_adjustment_inputs(params, make_points=True)
         self.get_camset(params).plot_np_array(obj_points.reshape((-1, 3)))
 
 